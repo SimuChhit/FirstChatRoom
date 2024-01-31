683eeb6c8ad3efac1f2e59a0dff4f0f41880a250
SimonChhit <simonchhit99@gmail.com>
2024-01-31 16:04:03 +0100

    [Major] Adding commands for explanation

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,4 +1,8 @@
+// Einbinden von Mongoose, einem ODM (Object Document Mapper) für MongoDB
 const mongoose = require('mongoose');
+
+// Erstellen eines Schemas für User-Daten
+// Ein Schema definiert die Struktur der Dokumente innerhalb einer MongoDB-Kollektion
 
 const userSchema = new mongoose.Schema({
     name: {type: String, require: true, minlegth: 3, maxlength: 30},
@@ -9,8 +13,10 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 }
 );
-
+// Erstellen eines Modells basierend auf dem Schema
+// Ein Modell stellt eine Sammlung in der MongoDB dar und wird verwendet, um Daten zu erstellen, abzurufen, zu aktualisieren und zu löschen.
 const userModel = mongoose.model("User", userSchema);
 
+// Exportieren des Modells, damit es in anderen Teilen der Anwendung verwendet werden kann
 module.exports = userModel;
 
--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,9 +1,16 @@
+// Importieren des Express-Moduls, das für das Routing und die Erstellung von Servern verwendet wird
 const express = require("express");
+
+// Importieren von Funktionen aus dem 'userController'.
+// Diese Funktionen definieren die Logik für verschiedene Endpunkte wie Registrierung, Login usw.
 const {registerUser, loginUser, findUser, getUsers} = require("../Controllers/userController");
 
+// Erstellen eines neuen Routers mit Express.
+// Ein Router ermöglicht es, Anfragen an verschiedene Pfade (Endpunkte) zu definieren und zu verwalten.
 const router = express.Router();
 
-
+// Definieren der Route für das Benutzerlogin.
+// Bei einer POST-Anfrage an '/login' wird die 'loginUser'-Funktion ausgeführt usw.
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
