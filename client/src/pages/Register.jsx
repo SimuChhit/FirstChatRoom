b89b8802fd5d80e4246788e97de051647359f601
SimonChhit <simonchhit99@gmail.com>
2024-01-31 16:38:26 +0100

    [Major] Change code for Authentication

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,30 +1,48 @@
+import {useContext} from "react";
 import {Alert, Button, Form, Row, Col, Stack} from "react-bootstrap";
+import './pageStyles/Register.css';
+import {FaUser} from "react-icons/fa";
+import {FaLock} from "react-icons/fa";
+import {FaEnvelope} from "react-icons/fa";
+import {AuthContext} from "../context/AuthContext.jsx";
+
 
 const Register = () => {
+
+  const {registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading} = useContext(AuthContext);
+
   return <>
-    <Form>
-      <Row style={{
-        height: "100vh",
-        justifyContent: "center",
-        paddingTop: "10%"
-      }}>
-        <Col xs={6}>
-          <Stack gap={3}>
-            <h2>
-              Register
-            </h2>
-            <Form.Control type="text" placeholder="Name"/>
-            <Form.Control type="email" placeholder="Email"/>
-            <Form.Control type="password" placeholder="password"/>
-            <Button variant="primary" type="submit">
-              Register
-            </Button>
-            <Alert variant="danger"><p>An error occured</p></Alert>
-          </Stack>
-        </Col>
-      </Row>
+    <Form onSubmit={registerUser}>
+      <div className="wrapper">
+        <h1>Register</h1>
+        <div className="input-box">
+          <Form.Control type="text" placeholder="name" onChange={(e) => updateRegisterInfo
+          ({...registerInfo, name: e.target.value})}/>
+          <FaUser className="icon"/>
+        </div>
+        <div className="input-box">
+          <Form.Control type="email" placeholder="Email" onChange={(e) => updateRegisterInfo
+          ({...registerInfo, email: e.target.value})}/>
+          <FaEnvelope className="icon"/>
+        </div>
+        <div className="input-box">
+          <Form.Control type="password" placeholder="Password" onChange={(e) => updateRegisterInfo
+          ({...registerInfo, password: e.target.value})}/>
+          <FaLock className="icon"/>
+        </div>
+        <div className="button-box">
+          <Button className="register-button" variant="primary" type="submit">
+            {isRegisterLoading ? "Creating your account" : "Register"}
+          </Button>
+        </div>
+        <div>
+          registerError?.error && <Alert variant="danger">
+          <p>{registerError?.message}</p>
+        </Alert>
+        </div>
+      </div>
     </Form>
-  </>;
+  </>
 };
 
 export default Register;
