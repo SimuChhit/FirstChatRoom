880ad62bf370c76fd6ca351f14bb10bba709f6e0
SimonChhit <simonchhit99@gmail.com>
2024-02-03 12:25:14 +0100

    [Add] changing routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,22 @@ import Login from "./pages/Login";
 import NavBar from "./components/NavBar.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Container} from "react-bootstrap";
+import {AuthContext} from "./context/AuthContext";
+import {useContext} from "react";
+
+
 
 function App() {
+  const {user} = useContext(AuthContext);
     return (
         <>
-          <div className="app-container">
+          <div className="Test">
             <NavBar/>
             <Container className="container-app">
                 <Routes>
-                    <Route path="/" element={<Chat/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/" element={user ? <Chat/>: <Login/>}/>
+                    <Route path="/register" element={user ? <Chat/>: <Register/>}/>
+                    <Route path="/login" element={user ? <Chat/>: <Login/>}/>
                     <Route path="*" element={<Navigate to="/"/>}/>
                 </Routes>
             </Container>
