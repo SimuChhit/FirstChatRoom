9ed500dbfaf0a23c7a2311e1b4675fb9d467273b
SimonChhit <simonchhit99@gmail.com>
2024-02-03 12:27:22 +0100

    [Add] small changes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,23 +1,37 @@
 import {Container, Nav, Navbar, Stack} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import './NavbarStyles.css';
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthContext.jsx";
 
 const NavBar = () => {
+  const {user, logoutUser} = useContext(AuthContext);
   return (
     <Navbar className="navbar-custom">
       <Container>
         <h2>
-          <Link to="/" className="link-light text-decoration-none">ChatApp</Link>
+          <Link to="/" className="link-light text-decoration-none">ChatRoom</Link>
         </h2>
-        <span className="Text-warning"></span>
+        <span className="Text-warning"> {user?.name}</span>
         <Nav>
           <Stack direction="horizontal" gap={3}>
-            <Link to="/login" className="link-light text-decoration-none">
-              Login
-            </Link>
-            <Link to="/register" className="link-light text-decoration-none">
-              Register
-            </Link>
+            {
+              user && (<>
+              <Link onClick={() => logoutUser()} to="/login" className="link-light text-decoration-none">
+                Logout
+              </Link>
+              </>)
+            }
+            {
+              !user && (<>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link to="/register" className="link-light text-decoration-none">
+                  Register
+                </Link>
+              </>)
+            }
           </Stack>
         </Nav>
       </Container>
