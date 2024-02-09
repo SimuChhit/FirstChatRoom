e25e1d9bcd5235bb2bba8ebc1e1635253f702dd0
Marcus Becker <exoticboymarcus@gmail.com>
2024-02-09 21:56:19 +0100

    List Chats and Creat Messages

---
+++ client/src/components/chat/PotentialChats.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { ChatContext } from "../../context/ChatContext";
+
+
+const PotentialChats = () => {
+    const { potentialChats } = useContext(ChatContext);
+    return (<>
+        <div className="all-users">
+            {potentialChats && potentialChats.map((u, index) => {
+                return (
+                    <div className="single-user" key={index}>
+                        {u.name}
+                        <span className="user-online"></span>
+                    </div>
+
+                );
+            })}
+        </div>
+    </>
+    );
+};
+
+export default PotentialChats;
