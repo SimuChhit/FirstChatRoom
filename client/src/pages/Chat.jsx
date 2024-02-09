3aaf85861fa339ded31b64d01a33b8df52aa9e0c
Marcus Becker <exoticboymarcus@gmail.com>
2024-02-09 21:48:08 +0100

    i List Chats and Create Chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,13 +1,38 @@
-import {ChatContext} from "../context/ChatContext.jsx";
-import {useContext} from "react";
+import { ChatContext } from "../context/ChatContext.jsx";
+import { useContext } from "react";
+import { Container } from "react-bootstrap";
+import UserChats from "../components/chat/UserChat.jsx";
+import PotentialChats from "../components/chat/PotentialChats.jsx";
 
 const Chat = () => {
-
-    const {userChats, isUserChatsLoading, userChatError} = useContext(ChatContext);
+    const { user } = useContext(AuthContext);
+    const { userChats, isUserChatsLoading, userChatError } = useContext(ChatContext);
 
     console.log("UserChats: ", userChats);
 
-    return <>hier wird unser Chat implementiert</>;
+    return (
+        <Container>
+            <PotentialChats />
+            {userChats?.length < 1 ? null : (
+                <Stack direction="horizontal" gap={4}
+                    className="algin-items-start">
+                    <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+                        {isUserChatsLoading && <p>Loading chats...</p>}
+                        {userChats?.map((chat, index) => {
+                            return (
+                                <div key={index}>
+                                    <UserChat chat={chat} user={user} />
+                                </div>
+                            )
+                        })}
+
+                    </Stack>
+                    <p>Chatbox</p>
+                </Stack>
+            )
+            }
+        </Container >
+    );
 };
 
 export default Chat;
